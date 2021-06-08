e035bf0db5346460036c49b51ccf8020bdbec760
MP3003K <mijailpalomino@upeu.edu.pe>
2021-06-08 18:35:43 -0500

    Correccion de errores de ruta

--- src/routes/detalle.routes.js
+++ src/routes/detalle.routes.js
@@ -4,12 +4,12 @@ const router = Router();
 
 import * as DetalleCtr from '../controllers/detalle.controller'
 const { checkToken } = require('../auth/token_validation');
-
-router.get('/',  DetalleCtr.readAlldetalle);
-router.get('/:id' , DetalleCtr.readDetalle);
 router.delete('/:id' , DetalleCtr.delDetalle);
 router.post('/', DetalleCtr.createDetalle);
 router.put('/:id', DetalleCtr.updateDetalle);
+
+router.get('/',  DetalleCtr.readAlldetalle);
+router.get('/:id' , DetalleCtr.readDetalle);
 
 
 export default router;
--- src/routes/venta.routes.js
+++ src/routes/venta.routes.js
@@ -4,12 +4,12 @@ const router = Router();
 
 import * as VentaCtr from '../controllers/venta.controller'
 const { checkToken } = require('../auth/token_validation');
+router.post('/',  VentaCtr.createVenta);
+router.put('/:id',  VentaCtr.updateVenta);
 
 router.get('/',  VentaCtr.readAllventa);
 router.get('/:id' , VentaCtr.readVenta);
 router.delete('/:id' , VentaCtr.delVenta);
 
-router.post('/',  VentaCtr.createVenta);
-router.put('/:id',  VentaCtr.updateVenta);
 
 export default router;
